8dd573dd721719044678c8dcd9048c091d4eb15e
Kurniawan1204 <kurniawanebot12@gmail.com>
2024-02-15 11:00:56 +0700

    add php

---
+++ 01_introduction_to_php/hello.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+    <link rel="stylesheet" href="../asset/bootstrap.min.css">
+  <link rel="stylesheet" href="index.css">
+</head>
+<body>
+<div class="card mb-3 mx-auto p-3 bg-info mt-3" style="max-width: 345px;">
+  <div class="row g-0  ">
+    <div class="col-md-4">
+      <img src="FOTO KURNIAWAN.jpeg" class=" img-fluid img-thumbnail bg-primary rounded-start" alt="...">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title text-white">IDENTITAS</h5>
+        <p class="card-text">Kurniawan XI RPL</p>
+        <p class="card-text"><small class="text-body-secondary">Tanggal pengumpulan : 06/02/2024</small></p>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="card border-dark mb-3 mx-auto p-3 mg-t-5 bg-dark bg-opacity-50" style="max-width: 25rem;">
+  <div class="card-header bg-secondary fs-4"><strong>PHP</strong></div>
+  <div class="card-body text-white">
+    <h5 class="card-title">Contoh Tampilan hello world di php</h5>
+<?php
+
+    // PHP adalah bahasa pemrograman scripting yang berjalan pada server.
+    // PHP Singkatan dari  
+    // PHP dikembangkan dengan bahasa C sehingga sintaksnya mirip-mirip dengan bahasa C.
+    // "<?php"ini adalah pembuka program PHP. Pembuka ini wajib ada di setiap program PHP.
+    // Contoh sederhana menampilkan Output Hello world ! di php
+    echo "Hello World !";  
+        
+    // Ada beberapa macam tipe data yang dapat disimpan dalam variabel:
+    // 1.Tipe data char (karakter)
+    // 2.Tipe data string (teks)
+    // 3.Tipe data integer (angka)
+    // 4.Tipe data float (pecahan)
+    // 5.Tipe data boolean
+    // 6.Tipe data objek
+    // 7.Tipe data Array
+    // 8.NULL
+
+?>
+ </div>
+ </div>
+ </div>
+</body>
+</html>
