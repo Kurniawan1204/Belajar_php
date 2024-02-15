c88a1500c9fa811a4116d942b271264eed51deb4
Kurniawan1204 <kurniawanebot12@gmail.com>
2024-02-15 11:03:10 +0700

    add php

---
+++ 03_echo_print/print.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+    <link rel="stylesheet" href="../asset/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+<body>
+<div class="card mb-3 mx-auto p-3 bg-info mt-3" style="max-width: 345px;">
+  <div class="row g-0  ">
+    <div class="col-md-4">
+      <img src="FOTO KURNIAWAN.jpeg" class=" img-fluid img-thumbnail bg-primary rounded-start" alt="...">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title text-white">IDENTITAS</h5>
+        <p class="card-text">Kurniawan XI RPL</p>
+        <p class="card-text"><small class="text-body-secondary">Tanggal pengumpulan : 06/02/2024</small></p>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="card border-dark mb-3 mx-auto p-3 mg-t-5 bg-dark bg-opacity-50" style="max-width: 18rem;">
+  <div class="card-header fs-4"><strong>Print</strong></div>
+  <div class="card-body text-light">
+    <h5 class="card-title"></h5>
+    <strong>Contoh :</strong>
+    <br>
+
+<?php
+$txt1 = "Learn PHP";
+$txt2 = "W3Schools.com";
+$x = 5;
+$y = 4;
+
+print "<h2>" . $txt1 . "</h2>";
+print "Study PHP at " . $txt2 . "<br>";
+print $x + $y;
+?>
+</div>
+</div>
+</div>
+
+
+</body>
+</html>
